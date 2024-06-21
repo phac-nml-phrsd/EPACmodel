7e6010bcb3f3002bdf7f1235703d0b7c5b226436
Irena Papst <irena.papst@phac-aspc.gc.ca>
2024-06-21 14:44:11 -0400

    make flow names clearer and update default values

--- inst/models/hosp/run-before-simulator.R
+++ inst/models/hosp/run-before-simulator.R
@@ -18,37 +18,46 @@ if(scenario.name == "change-contacts"){
 
 # decode parameters into model flows (use same rates for all ages)
 flow <- init_flow_vec(
-  epi_names = c("progression_to_I_R", "progression_to_I_H", "progression_to_I_D", "recovery_from_I_R", "hospitalization", "death_from_I_D", "recovery_from_H", "death_from_H"),
+  epi_names = c("progression_to_I_R", "progression_to_I_H", "progression_to_I_D",  "hospitalization", "death_from_I_D", "death_from_H","recovery_from_I_R", "recovery_from_H"),
   age_groups = age.group.lower
 )
 
 values$flow <- (flow 
   # progression of illness 
   |> update_flow(
-    pattern = "^progression",
-    value = 1/values$days_incubation/3
-  )
-  # recovery
-  |> update_flow(
-    pattern = "^recovery_from_I_R",
-    value = (1-values$prop_hosp-1/(1/values$prop_IFR_all - 1/values$prop_IFR_hosp))*1/values$days_infectious
+    pattern = "^progression_to_I_R\\.",
+    value = 1/values$days_incubation*(1-values$prop_hosp)*(1-values$prop_death_outside_hosp)
   )
   |> update_flow(
-    pattern = "^hospitalization",
-    value = values$prop_hosp*1/values$days_infectious
+    pattern = "^progression_to_I_H\\.",
+    value = 1/values$days_incubation*values$prop_hosp
   )
   |> update_flow(
-    pattern = "^death_from_I_D",
-    value = 1/(1/values$prop_IFR_all - 1/values$prop_IFR_hosp)*1/values$days_infectious
+    pattern = "^progression_to_I_D\\.",
+    value = 1/values$days_incubation*(1-values$prop_hosp)*values$prop_death_outside_hosp
   )
-  # outflows for H
+  # hospitalizations
   |> update_flow(
-    pattern = "^recovery_from_H",
-    value = (1-values$prop_IFR_hosp)*1/values$days_hosp
+    pattern = "^hospitalization\\.",
+    value = 1/values$days_infectious_I_H
+  )
+  # deaths
+  |> update_flow(
+    pattern = "^death_from_I_D\\.",
+    value = 1/values$days_infectious_I_D
   )
   |> update_flow(
-    pattern = "^death_from_H",
-    value = values$prop_IFR_hosp*1/values$days_hosp
+    pattern = "^death_from_H\\.",
+    value = 1/values$days_LOS_acute_care_to_death
+  )
+  # recoveries
+  |> update_flow(
+    pattern = "^recovery_from_I_R\\.",
+    value = 1/values$days_infectious_I_R
+  )
+  |> update_flow(
+    pattern = "^recovery_from_H\\.",
+    value = 1/values$days_LOS_acute_care_to_recovery
   )
 )
 
