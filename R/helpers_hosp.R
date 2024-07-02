228a92286e1c1a0bd2f97efe193a2f5642216ed0
Irena Papst <irena.papst@phac-aspc.gc.ca>
2024-07-02 11:13:41 -0400

    functions to compute individual and groups of hosp flows

--- R/helpers_hosp.R
+++ R/helpers_hosp.R
@@ -37,71 +37,151 @@ calculate_flow_hosp <- function(values){
 
     (flow 
         # progression of illness 
-        |> update_flow(
-            pattern = "^progression_to_I_R\\.",
-            value = 1/values$days_incubation*(1-values$prop_hosp)*(1-values$prop_nonhosp_death)
-        )
-        |> update_flow(
-            pattern = "^progression_to_I_A\\.",
-            value = 1/values$days_incubation*values$prop_hosp
-        )
-        |> update_flow(
-            pattern = "^progression_to_I_D\\.",
-            value = 1/values$days_incubation*(1-values$prop_hosp)*values$prop_nonhosp_death
+        |> update_flows_progression(
+            values$days_incubation, values$prop_hosp, values$prop_nonhosp_death
         )
         # leaving I_x states
         |> update_flow(
             pattern = "^recovery_from_I_R\\.",
-            value = 1/values$days_infectious_I_R
+            value = hosp_recovery_from_I_R(values$days_infectious_I_R)
         )
         |> update_flow(
             pattern = "^death_from_I_D\\.",
-            value = 1/values$days_infectious_I_D
+            value = hosp_recovery_from_I_D(values$days_infectious_I_D)
         )
         # entering acute care states
-        |> update_flow(
-            pattern = "^admission_to_A_R\\.",
-            value = 1/values$days_infectious_I_A*(1-values$prop_hosp_crit)*(1-values$prop_hosp_death)
-        )
-        |> update_flow(
-            pattern = "^admission_to_A_CR\\.",
-            value = 1/values$days_infectious_I_A*(values$prop_hosp_crit)*(1-values$prop_hosp_death)
-        )
-        |> update_flow(
-            pattern = "^admission_to_A_CD\\.",
-            value = 1/values$days_infectious_I_A*(values$prop_hosp_crit)*(values$prop_hosp_death)
-        )
-        |> update_flow(
-            pattern = "^admission_to_A_D\\.",
-            value = 1/values$days_infectious_I_A*(1-values$prop_hosp_crit)*(values$prop_hosp_death)
-        )
+        |> update_flows_admission_to_A(values$days_infectious_I_A, values$prop_hosp_crit, values$prop_hosp_death)
         # leaving acute care states
         |> update_flow(
             pattern = "^discharge_from_A_R\\.",
-            value = 1/values$days_LOS_acute_care_to_recovery
+            value = hosp_discharge_from_A_R(values$days_LOS_acute_care_to_recovery)
         )
         |> update_flow(
             pattern = "^admission_to_C_R\\.",
-            value = 1/values$days_LOS_acute_care_to_critical
+            value = hosp_admission_to_C_R(values$days_LOS_acute_care_to_critical)
         )
         |> update_flow(
             pattern = "^admission_to_C_D\\.",
-            value = 1/values$days_LOS_acute_care_to_critical
+            value = hosp_admission_to_C_D(values$days_LOS_acute_care_to_critical)
         )
         |> update_flow(
             pattern = "^death_from_A_D\\.",
-            value = 1/values$days_LOS_acute_care_to_death
+            value = hosp_death_from_A_D(values$days_LOS_acute_care_to_death)
         )
         # leaving critical care states
         |> update_flow(
             pattern = "^discharge_from_C_R\\.",
-            value = 1/values$days_LOS_critical_care_to_recovery
+            value = hosp_discharge_from_C_R(values$days_LOS_critical_care_to_recovery)
         )
         |> update_flow(
             pattern = "^death_from_C_D\\.",
-            value = 1/values$days_LOS_critical_care_to_death
+            value = hosp_death_from_C_D(values$days_LOS_critical_care_to_death)
         )
     )
+}
+
+# Formulas for calculating flows
+
+# flows as a group
+
+update_flows_progression <- function(flow, days_incubation, prop_hosp, prop_nonhosp_death, age_suffix = ""){
+    (flow
+        |> update_flow(
+            pattern = paste0("^progression_to_I_R\\.", age_suffix),
+            value = hosp_progression_to_I_R(days_incubation, prop_hosp, prop_nonhosp_death)
+        )
+        |> update_flow(
+            pattern = paste0("^progression_to_I_A\\.", age_suffix),
+            value = hosp_progression_to_I_A(days_incubation, prop_hosp)
+        )
+        |> update_flow(
+            pattern = paste0("^progression_to_I_D\\.", age_suffix),
+            value = hosp_progression_to_I_D(days_incubation, prop_hosp, prop_nonhosp_death)
+        )
+    )
+}
+
+update_flows_admission_to_A <- function(flow, days_infectious_I_A, prop_hosp_crit, prop_hosp_death, age_suffix = ""){
+    (flow
+        |> update_flow(
+            pattern = paste0("^admission_to_A_R\\.", age_suffix),
+            value = hosp_admission_to_A_R(days_infectious_I_A, prop_hosp_crit, prop_hosp_death)
+        )
+        |> update_flow(
+            pattern = paste0("^admission_to_A_CR\\.", age_suffix),
+            value = hosp_admission_to_A_CR(days_infectious_I_A, prop_hosp_crit, prop_hosp_death)
+        )
+        |> update_flow(
+            pattern = paste0("^admission_to_A_CD\\.", age_suffix),
+            value = hosp_admission_to_A_CD(days_infectious_I_A, prop_hosp_crit, prop_hosp_death)
+        )
+        |> update_flow(
+            pattern = paste0("^admission_to_A_D\\.", age_suffix),
+            value = hosp_admission_to_A_D(days_infectious_I_A, prop_hosp_crit, prop_hosp_death)
+        )
+    )
+}
+
+# individual flows
+
+hosp_progression_to_I_R <- function(days_incubation, prop_hosp, prop_nonhosp_death){
+    1/days_incubation*(1-prop_hosp)*(1-prop_nonhosp_death)
+}
+
+hosp_progression_to_I_A <- function(days_incubation, prop_hosp){
+    1/days_incubation*prop_hosp
+}
+
+hosp_progression_to_I_D <- function(days_incubation, prop_hosp, prop_nonhosp_death){
+    1/days_incubation*(1-prop_hosp)*prop_nonhosp_death
+}
+
+hosp_recovery_from_I_R <- function(days_infectious_I_R){
+    1/days_infectious_I_R
+}
+
+hosp_recovery_from_I_D <- function(days_infectious_I_D){
+    1/days_infectious_I_D
+}
+
+hosp_admission_to_A_R <- function(days_infectious_I_A, prop_hosp_crit, prop_hosp_death){
+    1/days_infectious_I_A*(1-prop_hosp_crit)*(1-prop_hosp_death)
+}
+
+hosp_admission_to_A_CR <- function(days_infectious_I_A, prop_hosp_crit, prop_hosp_death){
+    1/days_infectious_I_A*(prop_hosp_crit)*(1-prop_hosp_death)
+}
+
+hosp_admission_to_A_CD <- function(days_infectious_I_A, prop_hosp_crit, prop_hosp_death){
+    1/days_infectious_I_A*(prop_hosp_crit)*(prop_hosp_death)
+}
+
+hosp_admission_to_A_D <- function(days_infectious_I_A, prop_hosp_crit, prop_hosp_death){
+    1/days_infectious_I_A*(1-prop_hosp_crit)*(prop_hosp_death)
+}
+
+hosp_discharge_from_A_R <- function(days_LOS_acute_care_to_recovery){
+    1/days_LOS_acute_care_to_recovery
+}
+
+hosp_admission_to_C_R <- function(days_LOS_acute_care_to_critical){
+    1/days_LOS_acute_care_to_critical
+}
+
+hosp_admission_to_C_D <- function(days_LOS_acute_care_to_critical){
+    1/days_LOS_acute_care_to_critical
+}
+
+hosp_death_from_A_D <- function(days_LOS_acute_care_to_death){
+    1/days_LOS_acute_care_to_death
+}
+
+hosp_discharge_from_C_R <- function(days_LOS_critical_care_to_recovery){
+    1/days_LOS_critical_care_to_recovery
+}
+
+hosp_death_from_C_D <- function(days_LOS_critical_care_to_death){
+    1/days_LOS_critical_care_to_death
 }
 
 # For updating values in a simulator
