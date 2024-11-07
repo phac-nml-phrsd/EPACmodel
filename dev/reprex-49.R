02a348f6aab037b8b7400c738f2195f7cc5d360d
Irena Papst <irena.papst@phac-aspc.gc.ca>
2024-11-07 10:26:50 -0500

    update reprex

--- dev/reprex-49.R
+++ dev/reprex-49.R
@@ -10,18 +10,21 @@ sim.default <- simulate(simulator) # default values
 # adding new contact.pars
 
 values.default <- get_default_values(model.name)
-values <- c(
-    values.default,
-    list(
-        pop = rep(3e5, 17)
-    )
-)
+values <- values.default
+values$pop <- rep(3e5, 17)
+values$contact.pars <- mk_contact_pars(age.group.lower = seq(0,80,5), setting.weight = values$setting.weight, pop.new = values$pop)
 sim.newpars <- simulate(simulator, values = values)
 
-waldo::compare(sim.default, sim.newpars) # no differences, change to contact.pars not caught
+waldo::compare(sim.default, sim.newpars) # differences now!
 
 # what about changing a different value?
 
 values$transmissibility <- 0.01
 sim.newpars2 <- simulate(simulator, values = values)
 waldo::compare(sim.default, sim.newpars2) # lots of differences so change to transmissibility caught
+
+# what about updating values when simulator is built?
+simulator2 <- make_simulator(model.name, values = values)
+sim.newpars2 <- simulate(simulator2)
+waldo::compare(sim.default, sim.newpars2) # should be diff; is
+waldo::compare(sim.newpars, sim.newpars2) # should be the same; is
